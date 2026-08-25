75668c4c211f71cf5df5cec98aca4e09d6cccef5
odbayare <odbayar.enkh@gmail.com>
2026-08-25 13:20:44 +0800

    Run QPay callback regression in full test suite

--- tests/run-all.js
+++ tests/run-all.js
@@ -7,6 +7,7 @@ const tests = [
   "tests/direct-assessment-start.test.js",
   "tests/paid-first-flow.test.js",
   "tests/qpay-deeplink-ui.test.js",
+  "tests/qpay-mobile-handoff-callback.test.js",
   "tests/nonblocking-answer-save-runtime.test.js",
   "tests/nonblocking-answer-save-finalize-runtime.test.js",
   "tests/post-assessment-paywall-flow.test.js",
