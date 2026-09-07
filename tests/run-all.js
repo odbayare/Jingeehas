5ada84c6fe941404e7dbaed624813822f91f4e81
odbayare <odbayar.enkh@gmail.com>
2026-09-07 18:12:50 +0800

    Run V5 body-functional contract in main test suite

--- tests/run-all.js
+++ tests/run-all.js
@@ -28,6 +28,7 @@ const tests = [
   "tests/questionnaire-versioning.test.js",
   "tests/questionnaire-v3-safety-routing.test.js",
   "tests/hfe-v4-household-context.test.js",
+  "tests/v5-body-functional-context.test.js",
   "tests/assessment-flow-regression-versioned.test.js",
   "tests/accessibility-print.test.js",
   "tests/security-routing-metadata.test.js",
